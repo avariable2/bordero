85b2343fc39e909098597ff48a3da7504b0352a8
avariable2 <didileroVA1999@hotmail.fr>
2022-07-11 19:50:22 +0200

    feat: Ajout d'un stepper pour la creation d'une facture

--- lib/dialog/creation_facture.dart
+++ lib/dialog/creation_facture.dart
@@ -29,6 +29,7 @@ class _FormulaireCreationFactureState extends State<FormulaireCreationFacture> w
   late List<Client> listClients;
   List<Client> clientSelectionner = [];
   late List<bool> _selected;
+  int _index = 0;
   bool isLoading = false;
 
   final controllerChampNom = TextEditingController();
@@ -38,6 +39,9 @@ class _FormulaireCreationFactureState extends State<FormulaireCreationFacture> w
   final controllerChampVille = TextEditingController();
   final controllerChampNumero = TextEditingController();
   final controllerChampEmail = TextEditingController();
+
+  /// POUR CREER LE PDF
+  /// https://www.google.com/search?q=create+facture+flutter&rlz=1C1CHZN_frFR980FR980&oq=create+facture+flutter&aqs=chrome..69i57j0i22i30.8424j0j7&sourceid=chrome&ie=UTF-8#kpvalbx=_ITnMYuiTI4f_lwSe-o-YAw18
 
   Future _getListClients() async {
     setState(() => isLoading = true);
@@ -65,17 +69,35 @@ class _FormulaireCreationFactureState extends State<FormulaireCreationFacture> w
 
   @override
   Widget build(BuildContext context) {
-    return isLoading ? const Center(child: CircularProgressIndicator()) :  buildClient();
+    return isLoading ? const Center(child: CircularProgressIndicator()) : Stepper(
+      type: StepperType.horizontal,
+      currentStep: _index,
+      onStepCancel: () {
+        if (_index > 0) {
+          setState(() => _index -= 1);
+        }
+      },
+      onStepContinue: () {
+        if (_index <= 0) {
+          setState(() => _index += 1);
+        }
+      },
+      onStepTapped: (int index) {
+        setState(() => _index = index);
+      },
+      steps: [
+        Step(title: const Text("Client(s)"), isActive: _index >= 0, content: buildClient()),
+        Step(title: const Text("Séance(s)"), isActive: _index >= 1, content: buildSeance()),
+        Step(title: const Text("Facture"), isActive: _index >= 2, content: buildFacture())
+      ],
+    );
   }
 
   Widget buildClient() {
     return ListView(
-          physics: const BouncingScrollPhysics(),
-          padding: const EdgeInsets.only(
-          left: 20,
-          top: 20,
-          right: 20,
-        ),
+        shrinkWrap: true,
+        physics: const BouncingScrollPhysics(),
+        padding: const EdgeInsets.only(left: 0, top: 20, right: 0,),
         children: [
           const Text("Sélectionner client(s)",
             style: TextStyle(
@@ -108,15 +130,15 @@ class _FormulaireCreationFactureState extends State<FormulaireCreationFacture> w
                             title: Text("${listClients[i].prenom} ${listClients[i].nom} / ${listClients[i].adresse}"),
                             leading: const Icon(Icons.account_circle_sharp),
                             selected: _selected[i],
-                            onTap: () {
+                            onTap: () => setState(() => {
                               if (!clientSelectionner.contains(listClients[i])) {
-                                clientSelectionner.add(listClients[i]);
-                                _selected[i] = true;
+                              clientSelectionner.add(listClients[i]),
+                              _selected[i] = true,
                               } else {
-                                clientSelectionner.remove(listClients[i]);
-                                _selected[i] = false;
+                              clientSelectionner.remove(listClients[i]),
+                              _selected[i] = false,
                               }
-                            },
+                            })
                           ),
                       ],
                   ),
@@ -125,14 +147,6 @@ class _FormulaireCreationFactureState extends State<FormulaireCreationFacture> w
 
           const SizedBox(
             height: 15,
-          ),
-
-
-          ElevatedButton(
-            onPressed: () {
-
-            },
-            child: const Text('Continuer'),
           ),
 
         ],
@@ -365,227 +379,6 @@ class _FormulaireCreationFactureState extends State<FormulaireCreationFacture> w
   }
 
   Widget buildFacture() {
-    return SingleChildScrollView(
-      child: Form(
-        key: _formKey,
-        child:
-
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-
-            const SizedBox(
-              height: 15,
-            ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Expanded(flex: 1, child: Icon(Icons.info_outline)),
-                Expanded(flex: 4, child: Text('''Toutes les informations sont nécessaire pour la création d'un client.'''), ),
-              ],
-            ),
-
-            const SizedBox(
-              height: 15,
-            ),
-
-            const Divider(),
-
-            /* PARTIE NOM ET PRENOM */
-
-            Row(children: [
-              Expanded(child:
-              Padding(padding: const EdgeInsets.only( top:10, left: 8, bottom: 10),
-                child: TextFormField(
-                  controller: controllerChampNom,
-                  keyboardType: TextInputType.name,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Nom',
-                      icon: Icon(Icons.account_box_outlined)
-                  ),
-                  // The validator receives the text that the user has entered.
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Entrer un nom';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              ),
-
-              const SizedBox(
-                width: 20,
-              ),
-
-              Expanded(
-                child: Padding(padding: const EdgeInsets.only( top:10 ,right: 8, left: 8, bottom: 10),
-                  child: TextFormField(
-                    controller: controllerChampPrenom,
-                    keyboardType: TextInputType.name,
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Prénom',
-                        icon: Icon(Icons.account_box_outlined)),
-                    // The validator receives the text that the user has entered.
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Entrer un prénom';
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-              ),
-            ],
-            ),
-
-
-            /* PARTIE Adresse, code postal et ville */
-
-
-            Padding(padding: const EdgeInsets.only(right: 8, left: 8),
-              child:
-              TextFormField(
-                controller: controllerChampAdresse,
-                keyboardType: TextInputType.streetAddress,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Adresse',
-                    icon: Icon(Icons.person_pin_circle_outlined)),
-                // The validator receives the text that the user has entered.
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Entrer une adresse';
-                  }
-                  return null;
-                },
-              ),
-            ),
-
-
-
-
-            Row(children: [
-              Expanded(child:
-              Padding(padding: const EdgeInsets.only(top:10, right: 8, left: 8, bottom: 10),
-                child:
-                TextFormField(
-                  controller: controllerChampCodePostal,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Code postal',
-                      icon: Icon(Icons.domain_outlined)),
-                  // The validator receives the text that the user has entered.
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Entrer un code postal';
-                    }
-                    if (value.length != 5) {
-                      return 'Entrer un code postal valide';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              ),
-
-              const SizedBox(
-                width: 20,
-              ),
-
-              Expanded(
-                child:
-                Padding(padding: const EdgeInsets.only(top:10, right: 8, left: 8, bottom: 10),
-                  child:
-                  TextFormField(
-                    controller: controllerChampVille,
-                    keyboardType: TextInputType.streetAddress,
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Ville',
-                        icon: Icon(Icons.location_city_outlined)),
-                    // The validator receives the text that the user has entered.
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Entrer une ville';
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-              ),
-            ],
-            ),
-
-            /* PARTIE Numero de téléphone et EMAIl */
-
-
-            Padding(padding: const EdgeInsets.only( right: 8, left: 8),
-              child:
-              TextFormField(
-                controller: controllerChampNumero,
-                keyboardType: TextInputType.phone,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Numéro de téléphone',
-                    icon: Icon(Icons.phone_outlined)),
-                // The validator receives the text that the user has entered.
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Entrer un numéro de téléphone';
-                  }
-                  return null;
-                },
-              ),
-            ),
-
-            Padding(padding: const EdgeInsets.only(top:10, right: 8, left: 8),
-              child:
-              TextFormField(
-                controller: controllerChampEmail,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Email',
-                    icon: Icon(Icons.email_outlined)),
-                // The validator receives the text that the user has entered.
-                validator: (value) {
-                  if (value == null || value.isEmpty ) {
-                    return 'Entrer une email';
-                  }
-                  return null;
-                },
-              ),
-            ),
-
-            const SizedBox(
-              height: 20,
-            ),
-
-
-            ElevatedButton(
-              onPressed: () async {
-                // Validate returns true if the form is valid, or false otherwise.
-                if (_formKey.currentState!.validate()) {
-                  // If the form is valid, display a snackbar. In the real world,
-                  // you'd often call a server or save the information in a database.
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Traitement des données ...')),
-                  );
-
-                  //checkSiClientEstDejaSet();
-                }
-              },
-              child: const Text('Ajouter'),
-            ),
-
-          ],
-        ),
-      ),
-    );
+    return Container();
   }
 }
